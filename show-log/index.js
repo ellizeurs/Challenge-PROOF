5c2df5580246362adfa4f835b9980bc10d31cf94
ellizeurs <ellizeurs@gmail.com>
2023-01-22 12:42:16 -0300

    Def aux functions in index.js

--- show-log/index.js
+++ show-log/index.js
@@ -1,53 +1,53 @@
 require("dotenv").config();
 
 const amqplib = require('amqplib/callback_api');
-const queue = 'message';
+const queue = 'message'; // AMQP Channel
 
 start();
 
-function start() {
-    amqplib.connect(process.env.RABBITMQ_SERVER_ADDRESS,
+function start() { // Start AMQP Receiver
+    amqplib.connect(process.env.RABBITMQ_SERVER_ADDRESS, // Connect to AMQP Server
         {
-            credentials: require('amqplib').credentials.plain
+            credentials: require('amqplib').credentials.plain // Login in AMQP Server
                 (
                     process.env.RABBITMQ_USERNAME,
                     process.env.RABBITMQ_PASSWORD
                 )
         },
         (err, conn) => {
-            if (err) {
+            if (err) { // Test connection with AMQP Server
                 console.error("[AMQP]", err.message);
-                return setTimeout(start, 1000);
+                return setTimeout(start, 1000); // If error, retry in 1 second
             }
-            conn.on("error", function (err) {
+            conn.on("error", function (err) { // Catch AMQP Server error event
                 if (err.message !== "Connection closing") {
                     console.error("[AMQP] conn error", err.message);
+                    return setTimeout(start, 1000); // If error, retry in 1 second
                 }
             });
-            conn.on("close", function () {
+            conn.on("close", function () { // Catch AMQP Server close connection event
                 console.error("[AMQP] reconnecting");
-                return setTimeout(start, 1000);
+                return setTimeout(start, 1000); // If error, retry in 1 second
             });
 
+            // If connected
             console.log("[AMQP] connected");
 
-            if (!err)
-                conn.createChannel((err, ch) => {
-                    if (!err) {
-
-                        ch.assertQueue(queue);
-
-                        ch.consume(queue, (msg) => {
-                            if (msg !== null) {
-                                console.log(msg.content.toString());
-                                ch.ack(msg);
-                            } else {
-                                console.log('Consumer cancelled by server');
-                            }
-                        });
-                    }
-                });
+            conn.createChannel((err, ch) => receive_message(err, ch)); // Create channel at AMQP Server and call receive function
 
         });
 }
 
+function receive_message(err, ch){ 
+    if (!err) { // Verify channel state
+        ch.assertQueue(queue);
+        ch.consume(queue, (msg) => { // Start Receiver in this channel
+            if (msg !== null) { // Verify empty message
+                console.log(msg.content.toString());
+                ch.ack(msg);
+            } else {
+                console.log('Empty message');
+            }
+        });
+    }
+}
--- syslog/index.js
+++ syslog/index.js
@@ -4,73 +4,70 @@ const server = new SyslogServer();
 require("dotenv").config();
 
 const amqplib = require('amqplib/callback_api');
-const queue = 'message';
+const queue = 'message'; // AMQP Channel
 
-server.on("message", (value) => {
-    //console.log(value.date);     // the date/time the message was received
-    //console.log(value.host);     // the IP address of the host that sent the message
-    //console.log(value.protocol); // the version of the IP protocol ("IPv4" or "IPv6")
-    //console.log(value.message);  // the syslog message
+server.on("message", (value) => { // Event triggered when Syslog Server received a message
+    //value has date, host, protocol and message attributes
     try {
-        messageObj = JSON.parse(value.message);
-    } catch (e) {
-        //JSON error
-        //console.log(e);
+        messageObj = JSON.parse(value.message); //Parse received string to JSON
+    } catch (e) { //JSON parse error
+        console.log(e);
+        return; // End transmission to AMQP Server
     }
-    //console.log(messageObj);
-    amqplib.connect(process.env.RABBITMQ_SERVER_ADDRESS,
+    amqplib.connect(process.env.RABBITMQ_SERVER_ADDRESS, // Connect to AMQP Server
         {
-            credentials: require('amqplib').credentials.plain
+            credentials: require('amqplib').credentials.plain // Login in AMQP Server
                 (
                     process.env.RABBITMQ_USERNAME,
                     process.env.RABBITMQ_PASSWORD
                 )
         },
         (err, conn) => {
-            if (err) {
+            if (err) {  // Test connection with AMQP Server
                 console.error("[AMQP]", err.message);
                 return;
             }
-            conn.on("error", function (err) {
+            conn.on("error", function (err) { // Catch AMQP Server error event
                 if (err.message !== "Connection closing") {
                     console.error("[AMQP] conn error", err.message);
+                    return;
                 }
             });
-            conn.on("close", function () {
-                console.error("[AMQP] reconnecting");
+            conn.on("close", function () { // Catch AMQP Server close connection event
+                console.error("[AMQP] Close connection");
                 return;
             });
-            if (!err) {
-                // Sender
-                conn.createChannel((err, ch1) => {
-                    if (!err) {
 
-                        ch1.assertQueue(queue);
-                        ch1.sendToQueue(queue, Buffer.from(JSON.stringify(messageObj)));
-                    }
-                });
-            }
+            // If connected
+            conn.createChannel((err, ch) => sender_message(err, ch, messageObj)); // Create channel at AMQP Server and send message
+
         });
-
 });
 
-server.on("start", () => {
+function sender_message(err, ch, message){ 
+    if (!err) { // Verify channel create
+        ch.assertQueue(queue);
+        ch.sendToQueue(queue, Buffer.from(JSON.stringify(message))); // Start message transmission to AMQP Server
+    }
+}
+
+server.on("start", () => { // Catch Syslog Server start event
     console.log("Server start:");
-    console.log({
+    console.log({ // Show server info
         address: process.env.SYSLOG_SERVER_ADDRESS,
         port: process.env.SYSLOG_SERVER_PORT,
     });
 });
 
-server.on("stop", () => {
+server.on("stop", () => { // Catch Syslog Server stop event
     console.log("Server stop");
 });
 
-server.on("error", (e) => {
+server.on("error", (e) => { // Catch Syslog Server error event
     console.log(e);
 });
 
-server.start({
+server.start({ // Start Syslog Server
     address: process.env.SYSLOG_SERVER_ADDRESS,
     port: process.env.SYSLOG_SERVER_PORT,
 });
